7be3ccbdd56613957f2d9758f49337dfce3c648a
Chris C Cerami <chrisccerami@gmail.com>
2015-08-09 10:59:37 -0400

    Add check() to assert_array
    The check() function can be called for each test in order to deal with not only asserting the return, but also outputting progress information to the user.

--- src/assert_array.rs
+++ src/assert_array.rs
@@ -1,9 +1,24 @@
 pub fn run() {
-  assert!(empty(), "");
+  check(index());
+  check(empty());
 }
 
+fn check(ans: bool) {
+  assert!(ans, "Failure");
+  print!("✓");
+}
+
+// The elements of an array can be accessed by their indices
+// arr[4]
+fn index() -> bool {
+  let arr: [i32; 5] = [1, 2, 3, 4, 5];
+  arr__ == 1
+}
+
+// A new fixed size array can be created by declaring
+// the type of its elements along with its length
+// [i32; 0] = []
 fn empty() -> bool {
-  let arr: [i32; 0] = [];
-  let arr_two: [i32; 0] = [];
-  arr == arr_two
+  let arr: __;
+  arr.len() == 0
 }
